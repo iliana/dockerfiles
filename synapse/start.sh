f6dfdeaa68b86ce52221909b70a4ccef5cfcb130
Patrick Uiterwijk <puiterwijk@redhat.com>
2017-03-07 22:10:53 +0000

    Make synapse openshift-compatible by adding start.sh and EXPOSE
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

---
+++ synapse/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ ! -f /data/homeserver.yaml ];
+then
+    (
+        cd /data
+        python -m synapse.app.homeserver \
+            --generate-config \
+            --server-name $SERVER_NAME \
+            --keys-directory /data \
+            --generate-keys \
+            --config-path /data/homeserver.yaml \
+            --report-stats=no
+    )
+fi
+exec python -m synapse.app.homeserver --config-path /data/homeserver.yaml
